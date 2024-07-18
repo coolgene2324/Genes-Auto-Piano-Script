10f0f32739a02a6c2f17af9217c1a6c7c9576197
Gene T <coolgene2324@gmail.com>
2024-07-18 01:01:45 -0400

    Reverting to loopmidi

--- obj/Debug/net8.0-windows/apv1.1 c#.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/apv1.1 c#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apv1.1 c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d236201d681457a307c7a32f63424dec267e998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4aa6e99fb08403e8f843b3a1bc28b1c2baeb6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("apv1.1 c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apv1.1 c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
